8a5293efb9ea0e83d53a61bf91cae15fecc334ca
bertansezer <bertansezer@gmail.com>
2025-05-12 16:05:37 +0300

    third commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,8 +2,8 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
-        System.out.println("this is the project version 1");
-        System.out.println("helloooo");
+        System.out.println("this is the project version 3");
+
     }
 }
-//done with version one
+//done with version three
