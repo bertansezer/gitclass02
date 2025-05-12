d06d9bd6a8371e5a4772a83f3122e76aea4c4bb4
bertansezer <bertansezer@gmail.com>
2025-05-12 15:47:45 +0300

    second commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -3,6 +3,7 @@ public class test {
 
         System.out.println("Hello world");
         System.out.println("this is the project version 1");
+        System.out.println("helloooo");
     }
 }
 //done with version one
