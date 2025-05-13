447ecc330b1a1ca2aad948d01d265932661749e4
bertansezer <bertansezer@gmail.com>
2025-05-13 13:02:51 +0300

    fourth commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,8 +2,8 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
-        System.out.println("this is the project version 3");
+        System.out.println("this is the project version 4");
 
     }
 }
-//done with version three
+//done with version four
