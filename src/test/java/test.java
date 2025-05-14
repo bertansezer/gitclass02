3103a24678acaf34d86282ecb97d0cef517c4eac
bertansezer <bertansezer@gmail.com>
2025-05-14 16:06:59 +0300

    fifth commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,9 +1,9 @@
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
-        System.out.println("this is the project version 4");
+
+        System.out.println("this is the project version 5");
 
     }
 }
-//done with version four
+//done with version five
